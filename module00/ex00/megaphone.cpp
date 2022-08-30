80af0d962f873b024a743fbc770e93a5ff642ba5
Hildegard Vayon <hvayon@at-p7.msk.21-school.ru>
2022-08-30 20:59:59 +0300

    first commit

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    char input;
+    std::cin>>input;
+    //std::cout<<input>>;
+    input= int(input) - 32;
+    std::cout<<input;
+}
+
+//сделала большие буквы ыыы
